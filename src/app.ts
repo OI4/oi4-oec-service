6b124bd6dd22dbde53ba20087f9ed0f94adf7903
OI4-Hilscher <55870966+OI4-Hilscher@users.noreply.github.com>
2021-03-04 11:48:04 +0100

    Rename environment variable names

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,22 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 // Here, we get our configuration from Environment variables. If either of them is not specified, we use a provided .env file
-if (!(process.env.MQTT_BROKER_ADDRESS) ||
-  !(process.env.MQTT_PORT) ||
-  !(process.env.APPLICATION_INSTANCE_NAME) ||
+if (!(process.env.OI4_EDGE_MQTT_BROKER_ADDRESS) ||
+  !(process.env.OI4_EDGE_MQTT_SECURE_PORT) ||
+  !(process.env.OI4_EDGE_APPLICATION_INSTANCE_NAME) ||
   !(process.env.USE_HTTPS) ||
-  !(process.env.LOG_LEVEL)) {
+  !(process.env.OI4_EDGE_EVENT_LEVEL)) {
   dotenv.config({ path: path.join(__dirname, '.env') });
-  if (!(process.env.LOG_LEVEL) || !(process.env.LOG_LEVEL in ESubResource)) {
+  if (!(process.env.OI4_EDGE_EVENT_LEVEL) || !(process.env.OI4_EDGE_EVENT_LEVEL in ESubResource)) {
     console.log('Init: LOG_LEVEL either not specified or wrong enum value');
-    process.env.LOG_LEVEL = 'warn';
+    process.env.OI4_EDGE_EVENT_LEVEL = 'warn';
   }
 }
 
 const contState = new ContainerState();
 const busProxy = new OI4MessageBusProxy(contState);
 const webProxy = new OI4WebProxy(contState);
-const logger = new Logger(true, 'OI4-Base-Service', process.env.LOG_LEVEL as ESubResource, busProxy.mqttClient, busProxy.oi4Id, busProxy.serviceType);
+const logger = new Logger(true, 'OI4-Base-Service', process.env.OI4_EDGE_EVENT_LEVEL as ESubResource, busProxy.mqttClient, busProxy.oi4Id, busProxy.serviceType);
 logger.level = ESubResource.fatal;
 logger.log(`Testprint for level ${ESubResource.trace}`, ESubResource.trace);
 logger.log(`Testprint for level ${ESubResource.debug}`, ESubResource.debug);
