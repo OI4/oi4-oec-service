776da2962fbdd15e024c47f31994ee605ba19b6e
OI4-Hilscher <55870966+OI4-Hilscher@users.noreply.github.com>
2021-03-08 11:56:18 +0100

    Large changes in Logger in order to move from generic events to syslog events. We use winston for logging now, this means we log to messagebus on callback instead of immediately

--- src/Utilities/Logger/index.ts
+++ src/Utilities/Logger/index.ts
@@ -1,7 +1,12 @@
 import mqtt = require('async-mqtt'); /*tslint:disable-line*/
 import { OPCUABuilder } from '../OPCUABuilder/index';
-import { EContainerEventCategory, EGenericEventFilter, ESubResource, IContainerEvent } from '../../Models/IContainer';
+import { EContainerEventCategory, ESyslogEventFilter, IContainerEvent } from '../../Models/IContainer';
+import { Logger as WinstonLogger, transports } from 'winston';
+import winston = require('winston');
+import { Syslog, SyslogTransportInstance } from 'winston-syslog';
 
+// @ts-ignore TODO: this lib does not have any typings, but the api is simple enough
+const glossyParser = require('glossy').Parse;
 /**
  * Logger implementation.<br>
  * Adds several logging options, including levels and colors of the logs
@@ -12,20 +17,35 @@ class Logger {
    * @param {boolean} enabled - enables or disables logging
    * @param {number} level  - sets the minimum logging level
    */
-
+  private _syslogTransport: SyslogTransportInstance;
+  private _winstonLogger: WinstonLogger;
   private _enabled: boolean; /*tslint:disable-line*/
-  private _level: EGenericEventFilter; /*tslint:disable-line*/
+  private _level: ESyslogEventFilter; /*tslint:disable-line*/
   private _name: string; /*tslint:disable-line*/
   private _mqttClient?: mqtt.AsyncClient;
   private _oi4Id?: string;
   private _serviceType?: string;
   private _builder?: OPCUABuilder;
-  private readonly genericFilterToEnum = {
-    low: 0,
-    medium: 1,
-    high: 2,
-  };
-
+  private readonly syslogFilterToEnum = {
+    debug: 0,
+    informational: 1,
+    notice: 2,
+    warning: 3,
+    error: 4,
+    critical: 5,
+    alert: 6,
+    emergency: 7,
+  }
+  private readonly syslogToWinston = {
+    debug: 'debug',
+    informational: 'info',
+    notice: 'notice',
+    warning: 'warning',
+    error: 'error',
+    critical: 'crit',
+    alert: 'alert',
+    emergency: 'emerg',
+  }
   private readonly categoryToTopic = {
     CAT_SYSLOG_0: 'syslog',
     CAT_OPCSC_1: 'opcSC',
@@ -33,7 +53,7 @@ class Logger {
     CAT_GENERIC_99: 'generic',
   }
 
-  constructor(enabled: boolean = true, name: string, level: EGenericEventFilter = EGenericEventFilter.medium, mqttClient?: mqtt.AsyncClient, oi4Id?: string, serviceType?: string) {
+  constructor(enabled: boolean = true, name: string, level: ESyslogEventFilter = ESyslogEventFilter.warning, mqttClient?: mqtt.AsyncClient, oi4Id?: string, serviceType?: string) {
     /**
      * Enables or disables the logging. Default: `true`
      * @type {boolean}
@@ -61,6 +81,46 @@ class Logger {
     if (serviceType) {
       this._serviceType = serviceType;
     }
+    this._syslogTransport = new Syslog({ type: '5424'});
+    this._winstonLogger = winston.createLogger({
+      levels: winston.config.syslog.levels,
+      transports: [
+        new transports.Console(),
+        this._syslogTransport,
+      ]
+    })
+    this._winstonLogger.on('data', (data) => {
+      console.log(data);
+      const prod = this._syslogTransport.producer
+      const msg = prod.produce({
+        severity: data.level,
+        host: 'localhost',
+        date: new Date(),
+        message: data.message,
+      });
+      glossyParser.parse(msg, (parsedMessage: any) => {
+        let syslogDataMessage;
+        if (this._builder) {
+          const logPayload: IContainerEvent = {
+            category: EContainerEventCategory.CAT_SYSLOG_0,
+            number: parsedMessage.prival,
+            details: {
+              MSG: parsedMessage.message,
+              HEADER: `${parsedMessage.time.toISOString()} ${parsedMessage.host}`,
+            },
+          };
+          syslogDataMessage = this._builder.buildOPCUADataMessage([{ payload: logPayload }], new Date(), '543ae05e-b6d9-4161-a0a3-350a0fac5976'); /*tslint:disable-line*/
+          if (this._mqttClient) {
+            /* Optimistic log...if we want to be certain, we have to convert this to async */
+            this._mqttClient.publish(
+              `oi4/${this._serviceType}/${this._oi4Id}/pub/event/${this.categoryToTopic[EContainerEventCategory.CAT_SYSLOG_0]}/${data.level}`,
+              JSON.stringify(syslogDataMessage)
+            );
+          }
+        }
+    });
+
+      });
   }
 
   get enabled(): boolean {
@@ -77,7 +137,7 @@ class Logger {
   }
 
   set level(lvl) {
-    if (typeof lvl !== 'string') throw new Error('level must be of type string/EGenericFilter String');
+    if (typeof lvl !== 'string') throw new Error('level must be of type string/ESyslogLevel String');
     console.log(`Set logger level of ${this._name} level to: ${lvl}`);
     this._level = lvl;
   }
@@ -91,37 +151,47 @@ class Logger {
     this._name = newname;
   }
 
+  log(logstring: string, level: ESyslogEventFilter = ESyslogEventFilter.warning) {
+    if (this.enabled) {
+      if (this.syslogFilterToEnum[level] >= this.syslogFilterToEnum[this.level]) {
+        this._winstonLogger.log(this.syslogToWinston[level], `${this._name}: ${logstring}`);
+        }
+        winston.config.syslog.levels
+      }
+    return logstring;
+  }
+
   /**
    * Wrapper for console.log()
    * @param {string} logstring - string that is to be logged to the console
    * @param {string} color - either the chalk-color or the abbreviated version (e.g 'r' = chalk.red)
    * @param {number} level - the level that the log is to be logged to
    */
-  log(logstring: string, level = EGenericEventFilter.low, category: EContainerEventCategory = EContainerEventCategory.CAT_GENERIC_99,) {
-    if (this.enabled) {
-      if (this.genericFilterToEnum[level] >= this.genericFilterToEnum[this.level]) {
-        console.log(`${this._name}: ${logstring}`); // eslint-disable-line no-console
-        if (this._mqttClient) {
-          let logDataMessage;
-          if (this._builder) {
-            const logPayload: IContainerEvent = {
-              category: category,
-              number: 0,
-              description: logstring,
-              details: {
-                logLevel: level,
-                logOrigin: this._name,
-              }
-            };
-            logDataMessage = this._builder.buildOPCUADataMessage([{ payload: logPayload }], new Date(), '543ae05e-b6d9-4161-a0a3-350a0fac5976'); /*tslint:disable-line*/
-          }
-          const topicCategory = this.categoryToTopic[category];
-          /* Optimistic log...if we want to be certain, we have to convert this to async */
-          this._mqttClient.publish(`oi4/${this._serviceType}/${this._oi4Id}/pub/event/${topicCategory}/${level}/${this._oi4Id}`, JSON.stringify(logDataMessage));
-        }
-      }
-    }
-    return logstring;
-  }
+  // log(logstring: string, level = ESyslogEventFilter.debug, category: EContainerEventCategory = EContainerEventCategory.CAT_GENERIC_99) {
+  //   if (this.enabled) {
+  //     if (this.genericFilterToEnum[level] >= this.genericFilterToEnum[this.level]) {
+  //       console.log(`${this._name}: ${logstring}`); // eslint-disable-line no-console
+  //       if (this._mqttClient) {
+  //         let logDataMessage;
+  //         if (this._builder) {
+  //           const logPayload: IContainerEvent = {
+  //             category: category,
+  //             number: 0,
+  //             description: logstring,
+  //             details: {
+  //               logLevel: level,
+  //               logOrigin: this._name,
+  //             }
+  //           };
+  //           logDataMessage = this._builder.buildOPCUADataMessage([{ payload: logPayload }], new Date(), '543ae05e-b6d9-4161-a0a3-350a0fac5976'); /*tslint:disable-line*/
+  //         }
+  //         const topicCategory = this.categoryToTopic[category];
+  //         /* Optimistic log...if we want to be certain, we have to convert this to async */
+  //         this._mqttClient.publish(`oi4/${this._serviceType}/${this._oi4Id}/pub/event/${topicCategory}/${level}/${this._oi4Id}`, JSON.stringify(logDataMessage));
+  //       }
+  //     }
+  //   }
+  //   return logstring;
+  // }
 }
 export { Logger };
