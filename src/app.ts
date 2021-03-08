0d890eece7b2fde9ec673af59690e000bb44a737
OI4-Hilscher <55870966+OI4-Hilscher@users.noreply.github.com>
2021-03-08 11:56:31 +0100

    Change from generic logging to syslog logging

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { OI4MessageBusProxy } from './Proxy/Messagebus/index';
 import { OI4WebProxy } from './Proxy/Web/index';
 import { ContainerState } from './Container/index';
-import { EGenericEventFilter } from './Models/IContainer';
+import { ESyslogEventFilter } from './Models/IContainer';
 import { Logger } from './Utilities/Logger/index';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -13,18 +13,23 @@ if (!(process.env.OI4_EDGE_MQTT_BROKER_ADDRESS) ||
   !(process.env.USE_HTTPS) ||
   !(process.env.OI4_EDGE_EVENT_LEVEL)) {
   dotenv.config({ path: path.join(__dirname, '.env') });
-  if (!(process.env.OI4_EDGE_EVENT_LEVEL) || !(process.env.OI4_EDGE_EVENT_LEVEL in EGenericEventFilter)) {
+  if (!(process.env.OI4_EDGE_EVENT_LEVEL) || !(process.env.OI4_EDGE_EVENT_LEVEL in ESyslogEventFilter)) {
     console.log('Init: LOG_LEVEL either not specified or wrong enum value');
-    process.env.OI4_EDGE_EVENT_LEVEL = 'medium';
+    process.env.OI4_EDGE_EVENT_LEVEL = 'warning';
   }
 }
 
 const contState = new ContainerState();
 const busProxy = new OI4MessageBusProxy(contState);
 const webProxy = new OI4WebProxy(contState);
-const logger = new Logger(true, 'OI4-Base-Service', process.env.OI4_EDGE_EVENT_LEVEL as EGenericEventFilter, busProxy.mqttClient, busProxy.oi4Id, busProxy.serviceType);
-logger.level = EGenericEventFilter.high;
-logger.log(`Testprint for level ${EGenericEventFilter.low}`, EGenericEventFilter.low);
-logger.log(`Testprint for level ${EGenericEventFilter.medium}`, EGenericEventFilter.medium);
-logger.log(`Testprint for level ${EGenericEventFilter.high}`, EGenericEventFilter.high);
-logger.level = EGenericEventFilter.medium;
+const logger = new Logger(true, 'OI4-Base-Service', process.env.OI4_EDGE_EVENT_LEVEL as ESyslogEventFilter, busProxy.mqttClient, busProxy.oi4Id, busProxy.serviceType);
+logger.level = ESyslogEventFilter.emergency;
+logger.log(`Testprint for level ${ESyslogEventFilter.debug}`, ESyslogEventFilter.debug);
+logger.log(`Testprint for level ${ESyslogEventFilter.informational}`, ESyslogEventFilter.informational);
+logger.log(`Testprint for level ${ESyslogEventFilter.notice}`, ESyslogEventFilter.notice);
+logger.log(`Testprint for level ${ESyslogEventFilter.warning}`, ESyslogEventFilter.warning);
+logger.log(`Testprint for level ${ESyslogEventFilter.error}`, ESyslogEventFilter.error);
+logger.log(`Testprint for level ${ESyslogEventFilter.critical}`, ESyslogEventFilter.critical);
+logger.log(`Testprint for level ${ESyslogEventFilter.alert}`, ESyslogEventFilter.alert);
+logger.log(`Testprint for level ${ESyslogEventFilter.emergency}`, ESyslogEventFilter.emergency);
+logger.level = ESyslogEventFilter.warning;
