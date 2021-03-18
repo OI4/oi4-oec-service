d377a987ae3a8b4abf89a1039f2630f5bc91d300
OI4-Hilscher <55870966+OI4-Hilscher@users.noreply.github.com>
2021-03-18 09:52:19 +0100

    Move enums to separate folder

--- src/Models/IContainer.ts
+++ src/Models/IContainer.ts
@@ -1,4 +1,5 @@
 import { ISpecificContainerConfig } from '../Config/IContainerConfig';
+import { EDeviceHealth, EPublicationListConfig, EPublicationListExplicit, ESubscriptionListConfig } from '../Enums/EContainer';
 import { EOPCUABaseDataType } from '../Enums/EOPCUA';
 import { IOPCUANetworkMessage, IOPCUAMetaData, IMasterAssetModel, IOPCUALocalizedText } from './IOPCUA';
 
@@ -160,75 +161,6 @@ export interface IContainerState {
 
   // Methods
   addDataSet(dataname: string, data: IOPCUANetworkMessage, metadata: IOPCUAMetaData): void;
-}
-
-export enum ESubResource {
-  syslog = 'syslog',
-  opcSC = 'opcSC',
-  ne107 = 'ne107',
-  generic = 'generic',
-}
-
-export enum EGenericEventFilter {
-  low = 'low',
-  medium = 'medium',
-  high = 'high',
-}
-
-export enum ESyslogEventFilter {
-  emergency = 'emergency',
-  
-  alert = 'alert',
-  
-  critical = 'critical',
-  
-  error = 'error',
-  
-  warning = 'warning',
-  notice = 'notice',
-  informational = 'informational',
-  debug = 'debug',
-}
-
-export enum ENamurEventFilter {
-  normal = 'normal',
-  failure = 'failure',
-  checkFunction = 'checkFunction',
-  outOfSpecification = 'outOfSpecification',
-  maintenanceRequired = 'maintenanceRequired',
-}
-
-export enum EOpcUaEventFilter {
-  good = 'good',
-  uncertain = 'uncertain',
-  bad = 'bad',
-}
-
-export enum EDeviceHealth {
-  NORMAL_0 = 'NORMAL_0',
-  FAILURE_1 = 'FAILURE_1',
-  CHECK_FUNCTION_2 = 'CHECK_FUNCTION_2',
-  OFF_SPEC_3 = 'OFF_SPEC_3',
-  MAINTENANCE_REQUIRED_4 = 'MAINTENANCE_REQUIRED_4',
-}
-
-export enum EPublicationListExplicit {
-  EXPL_OFF_0 = 'EXPL_OFF_0',
-  EXPL_TAG_1 = 'EXPL_TAG_1',
-  EXPL_DSWID_2 = 'EXPL_DSWID_2',
-  EXPL_TAG_AND_DSWID_3 = 'EXPL_TAG_AND_DSWID_3'
-}
-
-export enum EPublicationListConfig {
-  NONE_0 = 'NONE_0',
-  STATUS_1 = 'STATUS_1',
-  INTERVAL_2 = 'INTERVAL_2',
-  STATUS_AND_INTERVAL_3 = 'STATUS_AND_INTERVAL_3',
-}
-
-export enum ESubscriptionListConfig {
-  NONE_0 = 'NONE_0',
-  CONF_1 = 'CONF_1',
 }
 
 export interface IDataSetClassIds {
