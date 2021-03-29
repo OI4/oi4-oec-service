4c959183fbcc3b6f6bc7d15fad5d0d213d96c745
OI4-Hilscher <55870966+OI4-Hilscher@users.noreply.github.com>
2021-03-29 14:03:11 +0200

    Adjust licenses

--- src/Container/index.ts
+++ src/Container/index.ts
@@ -105,15 +105,6 @@ class ContainerState extends ConfigParser implements IContainerState {
               licAddText: 'https://www.npmjs.com/package/express',
             },
             {
-              component: 'chalk',
-              licAuthors: [
-                'sindresorhus',
-                'Qix-',
-                'et al',
-              ],
-              licAddText: 'https://www.npmjs.com/package/chalk',
-            },
-            {
               component: 'uuid',
               licAuthors: [
                 'ctavan',
@@ -135,6 +126,106 @@ class ContainerState extends ConfigParser implements IContainerState {
               ],
               licAddText: 'https://www.npmjs.com/package/ajv',
             },
+            {
+              component: 'cors',
+              licAuthors: [
+                'dougwilson',
+                'troygoode',
+                'alex94cp',
+                'et al',
+              ],
+              licAddText: 'https://www.npmjs.com/package/cors',
+            },
+            {
+              component: 'cors',
+              licAuthors: [
+                'dougwilson',
+                'troygoode',
+                'alex94cp',
+                'et al',
+              ],
+              licAddText: 'https://www.npmjs.com/package/cors',
+            },
+            {
+              component: 'OEC-Registry',
+              licAuthors: [
+                'OI4-Hilscher',
+                'Berti Martens',
+              ],
+              licAddText: 'none',
+            },
+            {
+              component: 'winston',
+              licAuthors: [
+                'indexzero',
+                'chij',
+                'DABH',
+                'et al'
+              ],
+              licAddText: 'https://www.npmjs.com/package/winston',
+            },
+            {
+              component: 'winston-syslog',
+              licAuthors: [
+                'santigimeno',
+                'indexzero',
+                'DABH',
+                'et al'
+              ],
+              licAddText: 'https://www.npmjs.com/package/winston-syslog',
+            },
+            {
+              component: 'glossy',
+              licAuthors: [
+                'squeeks',
+                'fygrave',
+                'mattbornski',
+                'et al'
+              ],
+              licAddText: 'https://www.npmjs.com/package/glossy',
+            },
+            {
+              component: 'sequential-ask-queue',
+              licAuthors: [
+                'BalassaMarton'
+              ],
+              licAddText: 'https://www.npmjs.com/package/sequential-task-queue',
+            },
+            {
+              component: 'material-ui/core',
+              licAuthors: [
+                'material-ui organization'
+              ],
+              licAddText: 'https://www.npmjs.com/package/@material-ui/core',
+            },
+            {
+              component: 'material-ui/icons',
+              licAuthors: [
+                'material-ui organization'
+              ],
+              licAddText: 'https://www.npmjs.com/package/@material-ui/icons',
+            },
+            {
+              component: 'react',
+              licAuthors: [
+                'facebook'
+              ],
+              licAddText: 'https://www.npmjs.com/package/react',
+            },
+            {
+              component: 'react-dom',
+              licAuthors: [
+                'facebook'
+              ],
+              licAddText: 'https://www.npmjs.com/package/react-dom',
+            },
+            {
+              component: 'lodash',
+              licAuthors: [
+                'lodash'
+              ],
+              licAddText: 'https://www.npmjs.com/package/lodash',
+            },
           ],
         },
         {
@@ -152,20 +243,7 @@ class ContainerState extends ConfigParser implements IContainerState {
               licAddText: 'https://www.npmjs.com/package/dotenv',
             },
           ],
-        },
-        {
-          licenseId: 'BSD-3-Clause',
-          components: [
-            {
-              component: 'OI4-Registry',
-              licAuthors: [
-                'OI4-Hilscher',
-                'Berti Martens',
-              ],
-              licAddText: 'none',
-            },
-          ],
-        },
+        }
       ],
     };
 
@@ -216,33 +294,6 @@ class ContainerState extends ConfigParser implements IContainerState {
     CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
     OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
     OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`);
-    this.addLicenseText('BSD-3-Clause', `(BSD 3-Clause License)
-    All rights reserved.
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this
-       list of conditions and the following disclaimer.
-
-    2. Redistributions in binary form must reproduce the above copyright notice,
-       this list of conditions and the following disclaimer in the documentation
-       and/or other materials provided with the distribution.
-
-    3. Neither the name of the copyright holder nor the names of its
-       contributors may be used to endorse or promote products derived from
-       this software without specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    `);
 
     this._rtLicense = {};
 
