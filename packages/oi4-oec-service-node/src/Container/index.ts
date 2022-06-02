fc971a2127b9a59296b04e6fc371b26abea0f38b
RiccardoScuderi-EH <97168952+RiccardoScuderi-EH@users.noreply.github.com>
2022-06-02 10:19:57 +0200

    Oi4 200 (#41)
    * added mam file storage and reading
    
    * moved tests and documented
    
    * changed file name

--- packages/oi4-oec-service-node/src/Container/index.ts
+++ packages/oi4-oec-service-node/src/Container/index.ts
@@ -16,12 +16,15 @@ import {
 } from '@oi4/oi4-oec-service-model';
 
 import { IOPCUANetworkMessage, IOPCUAMetaData, IMasterAssetModel } from '@oi4/oi4-oec-service-opcua-model';
-
-import {ServiceMasterAssetModel} from '../Config/ServiceMasterAssetModel';
-
+import os from 'os';
 import { EOPCUALocale } from '@oi4/oi4-oec-service-opcua-model';
 import { EDeviceHealth, EPublicationListConfig, ESubscriptionListConfig } from '@oi4/oi4-oec-service-model';
-
+import {existsSync, readFileSync} from 'fs';
+import {ConfigFiles, MAMPathSettings} from '../Config/MAMPathSettings';
+/**
+ * class that initializes the container state
+ * Initializes the mam settings by a json file and build oi4id and Serialnumbers
+ * */
 class ContainerState extends ConfigParser implements IContainerState {
   public oi4Id: string; // TODO: doubling? Not needed here
   private readonly _profile: IContainerProfile;
@@ -37,13 +40,20 @@ class ContainerState extends ConfigParser implements IContainerState {
 
   public dataLookup: IContainerData;
   public metaDataLookup: IContainerMetaData;
-
+/**
+ * constructor that initializes the mam settings by retrieving the mam.json out of /etc/oi4/config/mam.json
+ * */
   constructor() {
     super();
 
-    this._mam = ServiceMasterAssetModel ; // Import MAM from JSON
+    this._mam = this.extractMamFile(`${MAMPathSettings.CONFIG_DIRECTORY}${ConfigFiles.mam}`); // Import MAM from JSON
+
+    if(this._mam === undefined) {
+      throw Error('MAM File not found');
+    }
+
     this._mam.Description.locale = EOPCUALocale.enUS; // Fill in container-specific values
-    this._mam.SerialNumber = process.env.OI4_EDGE_APPLICATION_INSTANCE_NAME as string;
+    this._mam.SerialNumber = os.hostname();
     this._mam.ProductInstanceUri = `${this._mam.ManufacturerUri}/${encodeURIComponent(this._mam.Model.text)}/${encodeURIComponent(this._mam.ProductCode)}/${encodeURIComponent(this._mam.SerialNumber)}`;
 
     this.oi4Id = this._mam.ProductInstanceUri;
@@ -332,6 +342,14 @@ class ContainerState extends ConfigParser implements IContainerState {
     }
 
   }
+
+  private extractMamFile(path: string): IMasterAssetModel  {
+    if(existsSync(path)){
+      return JSON.parse(readFileSync(path).toString());
+    }
+    return undefined;
+  }
+
   // Property accessor section
   get brokerState() {
     return this._brokerState;
