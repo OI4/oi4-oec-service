e2047648a30cdb05617e2474acbd4634a3dbbe02
vdedominicis <de.dominicis.valerio@gmail.com>
2022-06-08 15:20:23 +0200

    cleaned up the logger factory

--- packages/oi4-oec-service-node/tests/utils/mockedLoggerFactory.ts
+++ packages/oi4-oec-service-node/tests/utils/mockedLoggerFactory.ts
@@ -1,9 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-import * as winston from 'winston';
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-import {LoggingService} from './logger.service';
 import {Logger} from '@oi4/oi4-oec-service-logger';
 
 export type LoggerItems = {
@@ -14,7 +8,6 @@ export type LoggerItems = {
 export class MockedLoggerFactory {
 
     public static getLoggerItems(): LoggerItems {
-
         const fakeLogFile: Array<string> = [];
         // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
         // @ts-ignore
@@ -24,22 +17,6 @@ export class MockedLoggerFactory {
                 return str;
             }
         };
-
-        // trying to mock createLogger to return a specific logger instance
-        jest.mock('winston', () => ({
-            format: {
-                colorize: jest.fn(),
-                combine: jest.fn(),
-                label: jest.fn(),
-                timestamp: jest.fn(),
-                printf: jest.fn()
-            },
-            createLogger: jest.fn().mockReturnValue(fakeLogger),
-            transports: {
-                Console: jest.fn()
-            }
-        }));
-
         return {fakeLogger: fakeLogger, fakeLogFile: fakeLogFile}
     }
 
