b6490f8dc5a7821833b16ec0740caec144186a60
RiccardoScuderi-EH <97168952+RiccardoScuderi-EH@users.noreply.github.com>
2022-06-09 12:24:50 +0200

    deployed fake service that is trying to connect to broker

--- build_docker.sh
+++ build_docker.sh
@@ -1,10 +1,9 @@
 
-if [ -d ./packages/oi4-oec-service-demo/node_modules ]; then
-  rm -r ./packages/oi4-oec-service-demo/node_modules
-fi
+yarn build || {
+  exit 1
+}
 
 mkdir -p ./packages/oi4-oec-service-demo/node_modules/@oi4 && cp -L -R ./node_modules/@oi4/ ./packages/oi4-oec-service-demo/node_modules/@oi4
 
 docker build -t oi4-oec-service-demo -f ./packages/oi4-oec-service-demo/Dockerfile .
 
-rm -r ./packages/oi4-oec-service-demo/node_modules
