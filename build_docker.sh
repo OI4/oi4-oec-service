99e8a367bad6ef4ad813abf4dd38f609b49c4415
RiccardoScuderi-EH <97168952+RiccardoScuderi-EH@users.noreply.github.com>
2022-06-09 09:00:35 +0200

    splitted docker commands

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-yarn build || {
-               echo 'Failed to build project' ;
-               exit 1;
-           }
 
 if [ -d ./packages/oi4-oec-service-demo/node_modules ]; then
   rm -r ./packages/oi4-oec-service-demo/node_modules
@@ -11,7 +6,5 @@ fi
 mkdir -p ./packages/oi4-oec-service-demo/node_modules/@oi4 && cp -L -R ./node_modules/@oi4/ ./packages/oi4-oec-service-demo/node_modules/@oi4
 
 docker build -t oi4-oec-service-demo -f ./packages/oi4-oec-service-demo/Dockerfile .
-
-docker run -d --name oi4-service -it oi4-oec-service-demo
 
 rm -r ./packages/oi4-oec-service-demo/node_modules
