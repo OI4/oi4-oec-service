3a93b2b7cf494af8bda6804bf6cb792e8c238d62
vdedominicis <de.dominicis.valerio@gmail.com>
2022-06-09 12:53:10 +0200

    Created a factory for the OPCUABuilder

---
+++ packages/oi4-oec-service-node/tests/utils/MockedOPCUABuilderFactory.ts
@@ -0,0 +1,17 @@
+import {OPCUABuilder} from '@oi4/oi4-oec-service-opcua-model';
+
+export class MockedOPCUABuilderFactory {
+
+    public static mockOPCUABuilderMethod(methodName: any, mockedImplementation: Function) {
+        return jest
+            .spyOn(OPCUABuilder.prototype, methodName)
+            .mockImplementation((args: any) => {
+                return mockedImplementation.call(args);
+            });
+    }
+
+    public static getMockedOPCUABuilder(fakeOi4Id: string, fakeServiceType: string): OPCUABuilder {
+        return new OPCUABuilder(fakeOi4Id, fakeServiceType);
+    }
+
+}
