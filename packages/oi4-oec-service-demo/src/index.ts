fc5f2c8f47b3a10445e43578b199b640dc211428
RiccardoScuderi-EH <97168952+RiccardoScuderi-EH@users.noreply.github.com>
2022-06-10 09:20:19 +0200

    mapping for mam and mqtt_credentials for container

--- packages/oi4-oec-service-demo/src/index.ts
+++ packages/oi4-oec-service-demo/src/index.ts
@@ -4,5 +4,11 @@ import {OI4MessageBusProxy} from '@oi4/oi4-oec-service-node';
 const containerState = new ContainerState();
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-const oi4MessageBus = new OI4MessageBusProxy(containerState, {port: 8888, host: 'localhost'});
+const oi4MessageBus = new OI4MessageBusProxy(containerState, {
+    host: '172.17.0.4',
+    port: 8883,
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocol: 'mqtts',
+});
 console.log('===========FINISHED============');
