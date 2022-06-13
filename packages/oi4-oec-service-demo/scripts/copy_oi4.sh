92ebf45a9e69d8ed4bfb96eb3f81000023847f60
RiccardoScuderi-EH <97168952+RiccardoScuderi-EH@users.noreply.github.com>
2022-06-13 14:38:22 +0200

    added copy file script

---
+++ packages/oi4-oec-service-demo/scripts/copy_oi4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -L -R ../../node_modules/@oi4/ ./packages/oi4-oec-service-demo/node_modules/
--- packages/oi4-oec-service-demo/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-
-while true
-do
-  tail -f /dev/null & wait ${!}
-done
-
-exit 0
