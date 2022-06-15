e92f468495d656d6b3de72ceb1f90bef278bd5ff
vdedominicis <de.dominicis.valerio@gmail.com>
2022-06-15 11:25:03 +0200

    added more tests

--- packages/oi4-oec-service-node/tests/Utilities/Helpers/ClientPayloadHelper.test.ts
+++ packages/oi4-oec-service-node/tests/Utilities/Helpers/ClientPayloadHelper.test.ts
@@ -1,7 +1,7 @@
 import {ClientPayloadHelper} from '../../../dist/Utilities/Helpers/ClientPayloadHelper';
 import {LoggerItems, MockedLoggerFactory} from '../../Test-utils/Factories/MockedLoggerFactory';
 import {ValidatedPayload} from '../../../src/Utilities/Helpers/Types';
-import {EDeviceHealth, IContainerHealth, IContainerState} from '@oi4/oi4-oec-service-model';
+import {EDeviceHealth, EPublicationListExplicit, IContainerHealth, IContainerState} from '@oi4/oi4-oec-service-model';
 import {MockedIContainerStateFactory} from '../../Test-utils/Factories/MockedIContainerStateFactory';
 
 
@@ -61,7 +61,7 @@ describe('Unit test for ClientPayloadHelper', () => {
         expect(validatedPayload.payload).toBe(undefined);
     });
 
-    function createMockedPayload(dataSetWriterId: number, payload: any) {
+    function createLicenseMockedPayload(dataSetWriterId: number, payload: any) {
         return [{
             DataSetWriterId: dataSetWriterId,
             Payload: payload
@@ -71,7 +71,7 @@ describe('Unit test for ClientPayloadHelper', () => {
     it('createLicenseTextSendResourcePayload works when containerState.licenseText[filter] is not undefined', async () => {
         const validatedPayload: ValidatedPayload = clientPayloadHelper.createLicenseTextSendResourcePayload(mockedIContainerState, 'key', 'health');
         expect(validatedPayload.abortSending).toBe(false);
-        expect(validatedPayload.payload).toStrictEqual(createMockedPayload(2, {licenseText: 'fakeKey'}));
+        expect(validatedPayload.payload).toStrictEqual(createLicenseMockedPayload(2, {licenseText: 'fakeKey'}));
     });
 
     function createMockedPayloadWithSubresource(subResource: string, dataSetWriterId: number, payload: any) {
@@ -96,7 +96,60 @@ describe('Unit test for ClientPayloadHelper', () => {
         expect(validatedPayload.payload).toBe(undefined);
         expect(fakeLogFile.length).toBe(1);
         expect(fakeLogFile[0]).toBe('DataSetWriterId does not fit to licence Resource');
+    });
 
+    it('createLicenseSendResourcePayload works when dataSetWriterIdFilter is NaN', async () => {
+        const validatedPayload: ValidatedPayload = clientPayloadHelper.createLicenseSendResourcePayload(mockedIContainerState, '1', NaN, 'health');
+        expect(validatedPayload.abortSending).toBe(false);
+        const expectedInnerPayload = {components: [{licAddText: 'fakeLicence', component: 'fakeComponent', licAuthors: ['John Doe, Mary Poppins, Bilbo Baggins, John Rambo, Homer Simpson']}]};
+        const expectedPayload = createMockedPayloadWithSubresource('1',3, expectedInnerPayload);
+        expect(validatedPayload.payload).toStrictEqual(expectedPayload);
+    });
+
+    function createPublicationMockedPayload (resource: string, tag: string, datasetWriterId: number, oi4Id: string, active: boolean, explicit: string, interval: number, precisions: number, config: string) {
+        return {
+            resource: resource,
+            tag: tag,
+            DataSetWriterId: datasetWriterId,
+            oi4Identifier: oi4Id,
+            active: active,
+            explicit: explicit,
+            interval: interval,
+            precisions: precisions,
+            config: config,
+        }
+    }
+
+    it('createPublicationListSendResourcePayload works when dataSetWriterIdFilter is not NaN', async () => {
+        const validatedPayload: ValidatedPayload = clientPayloadHelper.createPublicationListSendResourcePayload(mockedIContainerState, 'oi4Id',2, 'health');
+        expect(validatedPayload.abortSending).toBe(false);
+        const expectedInnerPayload = createPublicationMockedPayload('fakeResource', 'fakeTag', -1, 'fakeOi4Identifier', false, EPublicationListExplicit.EXPL_OFF_0, -1, -1, 'NONE_0');
+        const expectedPayload = createMockedPayloadWithSubresource('fakeResource',2, expectedInnerPayload);
+        expect(validatedPayload.payload).toStrictEqual(expectedPayload);
+    });
+
+    it('createPublicationListSendResourcePayload works when dataSetWriterIdFilter is not NaN', async () => {
+        const validatedPayload: ValidatedPayload = clientPayloadHelper.createPublicationListSendResourcePayload(mockedIContainerState, 'oi4Id',2, 'health');
+        expect(validatedPayload.abortSending).toBe(false);
+        const expectedInnerPayload = createPublicationMockedPayload('fakeResource', 'fakeTag', -1, 'fakeOi4Identifier', false, EPublicationListExplicit.EXPL_OFF_0, -1, -1, 'NONE_0');
+        const expectedPayload = createMockedPayloadWithSubresource('fakeResource',2, expectedInnerPayload);
+        expect(validatedPayload.payload).toStrictEqual(expectedPayload);
+    });
+
+    it('createPublicationListSendResourcePayload works when dataSetWriterIdFilter is not NaN but dataSetWriterId does not match the resource', async () => {
+        const validatedPayload: ValidatedPayload = clientPayloadHelper.createPublicationListSendResourcePayload(mockedIContainerState, 'oi4Id',2, 'license');
+        expect(validatedPayload.abortSending).toBe(true);
+        expect(validatedPayload.payload).toBe(undefined);
+        expect(fakeLogFile.length).toBe(1);
+        expect(fakeLogFile[0]).toBe('DataSetWriterId does not fit to license Resource');
+    });
+
+    it('createPublicationListSendResourcePayload works when dataSetWriterIdFilter is NaN', async () => {
+        const validatedPayload: ValidatedPayload = clientPayloadHelper.createPublicationListSendResourcePayload(mockedIContainerState, 'fakeResource', NaN, 'health');
+        expect(validatedPayload.abortSending).toBe(false);
+        const expectedInnerPayload = createPublicationMockedPayload('fakeResource', 'fakeTag', -1, 'fakeOi4Identifier', false, EPublicationListExplicit.EXPL_OFF_0, -1, -1, 'NONE_0');
+        const expectedPayload = createMockedPayloadWithSubresource('fakeResource',2, expectedInnerPayload);
+        expect(validatedPayload.payload).toStrictEqual(expectedPayload);
     });
 
 });
