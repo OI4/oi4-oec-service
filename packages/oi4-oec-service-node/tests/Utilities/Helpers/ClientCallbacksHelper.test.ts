2b5147d03e61ad52431f1bd2584fb1c96debf2d0
vdedominicis <de.dominicis.valerio@gmail.com>
2022-06-17 10:54:48 +0200

    implemented test CallbackClient

--- packages/oi4-oec-service-node/tests/Utilities/Helpers/ClientCallbacksHelper.test.ts
+++ packages/oi4-oec-service-node/tests/Utilities/Helpers/ClientCallbacksHelper.test.ts
@@ -1,35 +1,77 @@
 import {LoggerItems, MockedLoggerFactory} from '../../Test-utils/Factories/MockedLoggerFactory';
 import {ClientPayloadHelper} from '../../../src/Utilities/Helpers/ClientPayloadHelper';
 import {ClientCallbacksHelper} from '../../../src/Utilities/Helpers/ClientCallbacksHelper';
-//import {IContainerState} from '@oi4/oi4-oec-service-model';
-//import {MockedIContainerStateFactory} from '../../Test-utils/Factories/MockedIContainerStateFactory';
+import {IContainerState} from '@oi4/oi4-oec-service-model';
+import {MockedIContainerStateFactory} from '../../Test-utils/Factories/MockedIContainerStateFactory';
+import mqtt from 'async-mqtt';
+import {MockedMqttClientFactory} from '../../Test-utils/Factories/MockedMqttClientFactory';
+import {MockedOPCUABuilderFactory} from '../../Test-utils/Factories/MockedOPCUABuilderFactory';
 
 describe('Unit test for ClientCallbackHelper', () => {
 
     const loggerItems: LoggerItems = MockedLoggerFactory.getLoggerItems();
     const fakeLogFile = loggerItems.fakeLogFile;
 
+    const mockedBuilder = MockedOPCUABuilderFactory.getMockedOPCUABuilder('fakeOi4Id', 'fakeServiceType');
+    const mockedMqttClient: mqtt.AsyncClient = MockedMqttClientFactory.getMockedClientWithDefaultImplementation();
     const clientPayloadHelper: ClientPayloadHelper = new ClientPayloadHelper(loggerItems.fakeLogger);
 
     let clientCallbackHelper: ClientCallbacksHelper;
-    //let mockedIContainerState: IContainerState;[[Prototype]] = Object
+    let mockedClient: IContainerState;
 
     beforeEach(() => {
         //Flush the messages log
         fakeLogFile.splice(0, fakeLogFile.length);
         clientCallbackHelper = new ClientCallbacksHelper(clientPayloadHelper, loggerItems.fakeLogger);
-        //mockedIContainerState = MockedIContainerStateFactory.getMockedContainerStateInstance()
+        mockedClient = MockedIContainerStateFactory.getMockedContainerStateInstance()
     });
 
-    function checkLog(msg: string) {
-        expect(fakeLogFile.length).toBe(1);
-        expect(fakeLogFile[0]).toBe(msg);
+    function checkLogEntries(size: number, messages: string[]) {
+        expect(fakeLogFile.length).toBe(size);
+        expect(fakeLogFile).toStrictEqual(messages);
+    }
+
+    function checkLogEntry(msg: string) {
+        checkLogEntries(1, [msg]);
     }
 
     it('onErrorCallback works', async () => {
         const err = new Error('whatever');
         await clientCallbackHelper.onErrorCallback(err);
-        checkLog(`Error in mqtt client: ${err}`);
+        checkLogEntry(`Error in mqtt client: ${err}`);
+    });
+
+    it('onCloseCallback works', async () => {
+        mockedClient.brokerState = true;
+
+        await clientCallbackHelper.onCloseCallback(mockedClient, mockedMqttClient, 'fakePreamble', 'fakeOi4Id', mockedBuilder);
+        expect(mockedMqttClient.publish).toHaveBeenCalled();
+        expect(mockedClient.brokerState).toBe(false);
+        checkLogEntry('Connection to mqtt broker closed');
+    });
+
+    it('onDisconnectCallback works', async () => {
+        mockedClient.brokerState = true;
+
+        await clientCallbackHelper.onDisconnectCallback(mockedClient);
+        expect(mockedClient.brokerState).toBe(false);
+        checkLogEntry('Disconnected from mqtt broker');
+    });
+
+    it('onReconnectCallback works', async () => {
+        mockedClient.brokerState = true;
+
+        await clientCallbackHelper.onReconnectCallback(mockedClient);
+        expect(mockedClient.brokerState).toBe(false);
+        checkLogEntry('Reconnecting to mqtt broker');
+    });
+
+    it('onClientConnectCallback works', async () => {
+        mockedClient.brokerState = false;
+
+        await clientCallbackHelper.onClientConnectCallback(mockedClient, mockedMqttClient, 'fakePreamble', 'fakeOi4Id', mockedBuilder);
+        expect(mockedClient.brokerState).toBe(true);
+        checkLogEntries(2, ['Connected successfully', 'Published Birthmessage on fakePreamble/pub/mam/fakeOi4Id']);
     });
 
 });
