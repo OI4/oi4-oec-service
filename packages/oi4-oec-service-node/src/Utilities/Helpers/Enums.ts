869f151fc074af9c12ea0111b00ca00fc3456d67
vdedominicis <de.dominicis.valerio@gmail.com>
2022-06-24 17:52:31 +0200

    removed no more needed enum

--- packages/oi4-oec-service-node/src/Utilities/Helpers/Enums.ts
+++ packages/oi4-oec-service-node/src/Utilities/Helpers/Enums.ts
@@ -29,13 +29,6 @@ export enum TopicMethods {
     DEL = 'del',
 }
 
-export enum PublishEventCategories {
-    CAT_SYSLOG_0 = 'CAT_SYSLOG_0',
-    CAT_STATUS_1 = 'CAT_STATUS_1',
-    CAT_NE107_2 = 'CAT_NE107_2',
-    CAT_GENERIC_99 = 'CAT_GENERIC_99',
-}
-
 export enum PayloadTypes {
     EMPTY = 'empty',
     LOCALE = 'locale',
