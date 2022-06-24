2f38df4aa477f9a416bf1e0c18820dc8a8dafee3
vdedominicis <de.dominicis.valerio@gmail.com>
2022-06-24 17:57:12 +0200

    removed no more useful types

--- packages/oi4-oec-service-node/src/Utilities/Helpers/Types.ts
+++ packages/oi4-oec-service-node/src/Utilities/Helpers/Types.ts
@@ -1,30 +1,4 @@
 import {IOPCUANetworkMessage, IOPCUAPayload} from '@oi4/oi4-oec-service-opcua-model';
-import {ENamurEventFilter} from '@oi4/oi4-oec-service-model';
-
-export type SyslogMessagePayloadDetails = {
-    'payload': {
-        'origin': string;
-        'number': number;
-        'description': string;
-        'category': string;
-        'details': any;
-    };
-}
-
-export type PublishEventMessagePayload = {
-    'payload': {
-        'origin': string;
-        'number': number;
-        'description': string;
-        'category': string;
-        'details': any;
-    };
-}
-
-export type NamurNe107State = {
-    value: number;
-    description: ENamurEventFilter;
-}
 
 export type ValidatedFilter = {
     isValid: boolean;
