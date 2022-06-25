2c98f37827be41662a938d7e900646885dc39bb1
vdedominicis <de.dominicis.valerio@gmail.com>
2022-06-25 11:12:36 +0200

    Added tests

--- packages/oi4-oec-service-node/tests/Utilities/Helpers/ClientPayloadHelper.test.ts
+++ packages/oi4-oec-service-node/tests/Utilities/Helpers/ClientPayloadHelper.test.ts
@@ -1,9 +1,17 @@
 import {ClientPayloadHelper} from '../../../src/Utilities/Helpers/ClientPayloadHelper';
 import {LoggerItems, MockedLoggerFactory} from '../../Test-utils/Factories/MockedLoggerFactory';
-import {NamurNe107State, PublishEventMessagePayload, ValidatedPayload} from '../../../src/Utilities/Helpers/Types';
-import {EDeviceHealth, ENamurEventFilter, IApplicationResources, IContainerHealth} from '@oi4/oi4-oec-service-model';
+import {ValidatedPayload} from '../../../src/Utilities/Helpers/Types';
+import {
+    CDataSetWriterIdLookup,
+    EDeviceHealth,
+    EventCategory,
+    IApplicationResources,
+    IContainerHealth,
+    ISyslogEvent
+} from '@oi4/oi4-oec-service-model';
 import {MockedIApplicationResourceFactory} from '../../Test-utils/Factories/MockedIApplicationResourceFactory';
-import {IOPCUAPayload} from "@oi4/oi4-oec-service-opcua-model";
+import {IOPCUAPayload} from '@oi4/oi4-oec-service-opcua-model';
+import {ResourceType} from '../../../src/Utilities/Helpers/Enums';
 
 
 describe('Unit test for ClientPayloadHelper', () => {
@@ -214,38 +222,24 @@ describe('Unit test for ClientPayloadHelper', () => {
         checkForUndefinedPayload(validatedPayload);
     });
 
-    function checkAgainstEDeviceHealthCase(key: EDeviceHealth, value: number, description: string) {
-        const state: NamurNe107State = clientPayloadHelper.getNamurNeStateDetails(key);
-        expect(state.value).toBe(value);
-        expect(state.description).toBe(description);
-    }
-
-    it('getNamurNeStateDetails works', async () => {
-        checkAgainstEDeviceHealthCase(EDeviceHealth.NORMAL_0, 0, ENamurEventFilter.normal);
-        checkAgainstEDeviceHealthCase(EDeviceHealth.FAILURE_1, 1, ENamurEventFilter.failure);
-        checkAgainstEDeviceHealthCase(EDeviceHealth.CHECK_FUNCTION_2, 2, ENamurEventFilter.checkFunction);
-        checkAgainstEDeviceHealthCase(EDeviceHealth.OFF_SPEC_3, 3, ENamurEventFilter.outOfSpecification);
-        checkAgainstEDeviceHealthCase(EDeviceHealth.MAINTENANCE_REQUIRED_4, 4, ENamurEventFilter.maintenanceRequired);
-    });
-
     it('createPublishEventMessage works', async() => {
-        const message: ValidatedPayload = clientPayloadHelper.createPublishEventMessage(42, 'fakeFilter', 'fakeSubResource', undefined);
-        expect(message.abortSending).toBe(false);
-        const payload: IOPCUAPayload = message.payload[0];
-        expect(payload.DataSetWriterId).toBe(42);
-        expect(payload.filter).toBe('fakeFilter');
-        expect(payload.subResource).toBe('fakeSubResource');
-        expect(payload.Payload).toBe(undefined);
-    });
-
-    it('createPublishEventMessagePayload works', async() => {
-        const messagePayload: PublishEventMessagePayload = clientPayloadHelper.createPublishEventMessagePayload('fakeOrigin', 42, 'fakeDescription', 'fakeCategory', {});
-        const payload = messagePayload.payload;
-        expect(payload.origin).toBe('fakeOrigin');
-        expect(payload.number).toBe(42);
-        expect(payload.description).toBe('fakeDescription');
-        expect(payload.category).toBe('fakeCategory');
-        expect(payload.details).toStrictEqual({});
+        const event: ISyslogEvent = {
+            origin: 'fakeOrigin',
+            number: 0,
+            description: 'fakeDescription',
+            category: EventCategory.CAT_SYSLOG_0,
+            details: {
+                MSG: 'fakeMSG',
+                HEADER: 'fakeHeader',
+            }
+        }
+        const message: IOPCUAPayload[] = clientPayloadHelper.createPublishEventMessage('fakeFilter', 'fakeSubResource', event);
+        expect(message.length).toBe(1);
+        const extractedMessage = message[0];
+        expect(extractedMessage.DataSetWriterId).toBe(CDataSetWriterIdLookup[ResourceType.EVENT]);
+        expect(extractedMessage.filter).toBe('fakeFilter');
+        expect(extractedMessage.subResource).toBe('fakeSubResource');
+        expect(extractedMessage.Payload).toStrictEqual(event);
     });
 
 });
