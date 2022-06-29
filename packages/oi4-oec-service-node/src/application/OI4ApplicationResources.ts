73f8d02c6cbbba4d40531ae7c1643b0ae1d3742b
Thomas Weinschenk <thomas.weinschenk@endress.com>
2022-06-29 16:00:43 +0200

    Applied changes from remote

--- packages/oi4-oec-service-node/src/application/OI4ApplicationResources.ts
+++ packages/oi4-oec-service-node/src/application/OI4ApplicationResources.ts
@@ -43,7 +43,7 @@ class OI4ApplicationResources extends ConfigParser implements IOI4ApplicationRes
     /**
      * constructor that initializes the mam settings by retrieving the mam.json out of /etc/oi4/config/mam.json
      * */
-    constructor(mamFile: string = `${MAMPathSettings.CONFIG_DIRECTORY}${ConfigFiles.mam}`) {
+    constructor(mamFile = `${MAMPathSettings.CONFIG_DIRECTORY}${ConfigFiles.mam}`) {
         super();
 
         this._mam = OI4ApplicationResources.extractMamFile(mamFile); // Import MAM from JSON
@@ -76,56 +76,6 @@ class OI4ApplicationResources extends ConfigParser implements IOI4ApplicationRes
         };
 
         this.health = new Health(EDeviceHealth.NORMAL_0, 100);
-
-        this.license = [new License('', [])];
-
-        this.licenseText = {};
-        this.addLicenseText('MIT', `(The MIT License)
-    Copyright (c) 2009-2014 TJ Holowaychuk <tj@vision-media.ca>
-    Copyright (c) 2013-2014 Roman Shtylman <shtylman+expressjs@gmail.com>
-    Copyright (c) 2014-2015 Douglas Christopher Wilson <doug@somethingdoug.com>
-
-    Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the
-    'Software'), to deal in the Software without restriction, including
-    without limitation the rights to use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of the Software, and to
-    permit persons to whom the Software is furnished to do so, subject to
-    the following conditions:
-
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`);
-
-        this.addLicenseText('BSD-2-Clause', `(BSD 2-Clause License)
-    All rights reserved.
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this
-      list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above copyright notice,
-      this list of conditions and the following disclaimer in the documentation
-      and/or other materials provided with the distribution.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`);
 
         this.rtLicense = new RTLicense();
 
@@ -226,6 +176,20 @@ class OI4ApplicationResources extends ConfigParser implements IOI4ApplicationRes
 
     get license(): License[] {
         return this._license;
+    }
+
+    getLicense(oi4Id: string, licenseId?: string): License[] {
+        if(oi4Id === undefined) {
+            return this.license;
+        } else if (oi4Id !==  this.oi4Id){
+            throw new Error('Sub resources not yet implemented');
+        }
+
+        if(licenseId === undefined) {
+            return this.license;
+        }
+
+        return this.license.filter((elem: License) => elem.licenseId === licenseId ? elem : null);
     }
 
     private set license(license) {
