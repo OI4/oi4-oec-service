32361365113453b450395ad26b12a39a4f31a8b8
vdedominicis <de.dominicis.valerio@gmail.com>
2022-07-12 15:34:24 +0200

    Added some tests

--- packages/oi4-oec-service-node/tests/Utilities/Helpers/TopicParser.test.ts
+++ packages/oi4-oec-service-node/tests/Utilities/Helpers/TopicParser.test.ts
@@ -6,14 +6,14 @@ import {TopicInfo} from '../../../dist/Utilities/Helpers/Types';
 describe('Unit test for TopicParser', () => {
 
     const defaultFakeAppId = 'mymanufacturer.com/1/1/1';
-    //const defaultFakeSubResource = 'fakeSubResource';
+    const defaultFakeSubResource = 'fakeSubResource';
     const defaultTopicPrefix = 'fake/Registry';
     const defaultFakeLicenseId = '1234';
     const defaultFakeFilter = 'oi4_pv';
     const defaultFakeOi4Id = '2/2/2/2';
     const fakeCategory = 'fakeCategory';
     const fakeFilter = 'fakeFilter';
-    //const defaultFakeTag = 'tag';
+    const defaultFakeTag = 'tag';
 
     it('If the appId is invalid an error is thrown', async () => {
         const topic = `${defaultTopicPrefix}/mymanufacturer.com//1/1/${TopicMethods.GET}/${Resource.MAM}`;
@@ -71,6 +71,16 @@ describe('Unit test for TopicParser', () => {
         checkFilter(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.METADATA}/${defaultFakeOi4Id}/${defaultFakeFilter}`);
     });
 
+    function checkFilterWithError(topic: string) {
+        expect(() => {TopicParser.parseTopic(topic)}).toThrowError('Invalid filter: ');
+    }
+
+    it('In case of config, data and metadata, invalid filter generates error', async () => {
+        checkFilterWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.CONFIG}/${defaultFakeOi4Id}/`);
+        checkFilterWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.DATA}/${defaultFakeOi4Id}/`);
+        checkFilterWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.METADATA}/${defaultFakeOi4Id}/`);
+    });
+
     function checkLicense(topic: string) {
         const info: TopicInfo = TopicParser.parseTopic(topic);
         expect(info.licenseId).toStrictEqual(defaultFakeLicenseId);
@@ -79,6 +89,50 @@ describe('Unit test for TopicParser', () => {
     it('In case of config, data and metadata, filter is properly extracted', async () => {
         checkLicense(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.LICENSE}/${defaultFakeOi4Id}/${defaultFakeLicenseId}`);
         checkLicense(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.LICENSE_TEXT}/${defaultFakeOi4Id}/${defaultFakeLicenseId}`);
+    });
+
+    function checkLicenseWithError(topic: string) {
+        expect(() => {TopicParser.parseTopic(topic)}).toThrowError('Invalid licenseId: ');
+    }
+
+    it('In case of config, data and metadata, invalid filter generates error', async () => {
+        checkLicenseWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.LICENSE}/${defaultFakeOi4Id}/`);
+        checkLicenseWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.LICENSE_TEXT}/${defaultFakeOi4Id}/`);
+    });
+
+    function checkList(topic: string, withTag = false) {
+        const info: TopicInfo = TopicParser.parseTopic(topic);
+        expect(info.subResource).toStrictEqual(defaultFakeSubResource);
+        if(withTag) {
+            expect(info.filter).toStrictEqual(`${defaultFakeSubResource}/${defaultFakeTag}`);
+            expect(info.tag).toStrictEqual(defaultFakeTag);
+        } else {
+            expect(info.filter).toStrictEqual(defaultFakeSubResource);
+        }
+    }
+
+    it('In case of publicationList and subscriptionList, subresource and filter are properly extracted', async () => {
+        checkList(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.PUBLICATION_LIST}/${defaultFakeOi4Id}/${defaultFakeSubResource}`);
+        checkList(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.SUBSCRIPTION_LIST}/${defaultFakeOi4Id}/${defaultFakeSubResource}`);
+
+        checkList(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.PUBLICATION_LIST}/${defaultFakeOi4Id}/${defaultFakeSubResource}/${defaultFakeTag}`, true);
+        checkList(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.SUBSCRIPTION_LIST}/${defaultFakeOi4Id}/${defaultFakeSubResource}/${defaultFakeTag}`, true);
+    });
+
+    function checkListWithError(topic: string, withTag = false) {
+        if(withTag) {
+            expect(() => {TopicParser.parseTopic(topic)}).toThrowError('Invalid tag: ');
+        } else {
+            expect(() => {TopicParser.parseTopic(topic)}).toThrowError('Invalid subresource: ');
+        }
+    }
+
+    it('In case of publicationList and subscriptionList, subresource and filter are properly extracted', async () => {
+        checkListWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.PUBLICATION_LIST}/${defaultFakeOi4Id}/`);
+        checkListWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.SUBSCRIPTION_LIST}/${defaultFakeOi4Id}/`);
+
+        checkListWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.PUBLICATION_LIST}/${defaultFakeOi4Id}/${defaultFakeSubResource}/`, true);
+        checkListWithError(`${defaultTopicPrefix}/${defaultFakeAppId}/${TopicMethods.GET}/${Resource.SUBSCRIPTION_LIST}/${defaultFakeOi4Id}/${defaultFakeSubResource}/`, true);
     });
 
 });
