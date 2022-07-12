690b1b786ce6f78475c04891094174423ac08c5a
vdedominicis <de.dominicis.valerio@gmail.com>
2022-07-12 11:34:06 +0200

    removed useless docu

--- packages/oi4-oec-service-node/src/Utilities/Helpers/TopicValidator.ts
+++ packages/oi4-oec-service-node/src/Utilities/Helpers/TopicValidator.ts
@@ -4,57 +4,6 @@ import {TopicWrapper} from "./Types";
 
 export class TopicValidator {
 
-    /**
-     Accordingly to the guideline, these are the possible generic requests
-
-     - oi4/<serviceType>/<appId>/pub/event                                      /<category>/<filter>
-     --- length = 10 -> ALL fields are mandatory
-
-     - oi4/<serviceType>/<appId>/{get/pub}/mam                                  /<oi4Identifier>?
-     - oi4/<serviceType>/<appId>/{get/pub}/health                               /<oi4Identifier>?
-     - oi4/<serviceType>/<appId>/{get/pub}/rtLicense                            /<oi4Identifier>?
-     - oi4/<serviceType>/<appId>/{get/pub}/profile                              /<oi4Identifier>?
-     --- length = 8 -> no oi4Id
-     --- length = 12 -> yes Oi4Id
-
-     - oi4/<serviceType>/<appId>/{get/pub}/referenceDesignation                 /<oi4Identifier>?
-     - oi4/<serviceType>/<appId>/{get}/interfaces                               /<oi4Identifier>?
-     --- length = 8 -> no oi4Id
-     --- length = 12 -> yes Oi4Id
-     - oi4/<serviceType>/<appId>/{set/del}/referenceDesignation                 /<oi4Identifier>?
-     - oi4/<serviceType>/<appId>/pub/interfaces                                 /<oi4Identifier>?
-     --- length = 12 -> yes Oi4Id
-
-     - oi4/<serviceType>/<appId>/{get/pub}/config                               /<oi4Identifier>?/<filter>?
-     - oi4/<serviceType>/<appId>/{get/pub}/data                                 /<oi4Identifier>?/<filter>?
-     --- length = 8 -> no oi4Id and no filter
-     --- length = 12 -> yes Oi4Id but no filter
-     --- length = 13 -> yes oi4Id and yes filter
-     - oi4/<serviceType>/<appId>/set/config                                     /<oi4Identifier>?/<filter>?
-     - oi4/<serviceType>/<appId>/set/data                                       /<oi4Identifier>?/<filter>?
-     --- length = 13 -> ALL fields are mandatory
-
-     - oi4/<serviceType>/<appId>/{get/set/pub}/metadata                         /<oi4Identifier>?/<filter>?
-     --- length = 13 -> ALL fields are mandatory
-
-     - oi4/<serviceType>/<appId>/{get/pub}/license                              /<oi4Identifier>?/<licenseId>?
-     - oi4/<serviceType>/<appId>/{get/pub}/licenseText                          /<oi4Identifier>?/<licenseId>?
-     --- length = 8 -> no oi4Id and no licenseId
-     --- length = 12 -> yes Oi4Id but no licenseId
-     --- length = 13 -> yes oi4Id and yes licenseId
-
-     - oi4/<serviceType>/<appId>/{get/pub}/publicationList                      /<oi4Identifier>?/<resourceType>?/<tag>?
-     - oi4/<serviceType>/<appId>/{get/pub}/subscriptionList                     /<oi4Identifier>?/<resourceType>?/<tag>?
-     --- length = 8 -> no oi4Identifier and no resourceType and no tag
-     --- length = 12 -> yes oi4Identifier and no resourceType and no tag
-     --- length = 13 -> yes oi4Identifier, yes resourceType and no tag
-     --- length = 14 -> yes oi4Identifier, yes resourceType and yes tag
-     - oi4/<serviceType>/<appId>/set/publicationList                            /<oi4Identifier>?/<resourceType>?/<tag>?
-     - oi4/<serviceType>/<appId>/{set/del}/subscriptionList                     /<oi4Identifier>?/<resourceType>?/<tag>?
-     --- length = 13 -> yes oi4Identifier, yes resourceType and no tag
-     --- length = 14 -> yes oi4Identifier, yes resourceType and yes tag
-     */
-
     static isMalformedTopic(wrapper: TopicWrapper) {
         const method: TopicMethods = wrapper.topicInfo.method;
         const resource: Resource = wrapper.topicInfo.resource;
