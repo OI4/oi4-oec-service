7047731edb82bdf62cbcc3d1b5c271f75ec05a0b
Christian Franz <103026556+chrfranz@users.noreply.github.com>
2022-07-19 15:01:43 +0200

    Deleted IContainerConfig.ts (#97)

--- packages/oi4-oec-service-model/src/index.ts
+++ packages/oi4-oec-service-model/src/index.ts
@@ -4,7 +4,6 @@ export * from './model/DatatSetClassIds';
 export * from './model/Resource';
 export * from './model/IContainer';
 export * from './model/IEvent';
-export * from './model/IContainerConfig';
 export * from './model/Profiles';
 export * from './model/Payload';
 export * from './model/IOI4ApplicationResources'
