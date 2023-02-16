1687878a2a71bb36dacf3a93c83bde58469fe5cb
Sebastian Heinze <sebastian.heinze@burkert.com>
2023-02-16 16:32:51 +0000

    added private functions for replacing chars

--- packages/oi4-oec-dnp-encoding/src/main.ts
+++ packages/oi4-oec-dnp-encoding/src/main.ts
@@ -32,13 +32,14 @@ const prohibitedPrintableChars: string[] = [
     '}',
 ];
 
+
 /**
- * Masks all characters forbidden by DIN 91406
- * @param input string to encode into dnp-encoding
- * @returns string with all reserved & prohibited printable characters masked
+ * Masks all reserved characters in a given string
+ * @param input string to replace all dnp reserved chars
+ * @returns string with all reserved chars masked
  */
-export function encode(input: string): string {
-    let output = input;
+function replaceReservedChars(input: string): string {
+    let output: string = input;
 
     // replace each found problematic character with its decimal representation of the us-ascii code, starting with an ,=44
     reservedChars.forEach((char) => {
@@ -47,6 +48,19 @@ export function encode(input: string): string {
             `,${char.charCodeAt(0).toString(16).toUpperCase()}`,
         );
     });
+
+    return output;
+}
+
+/**
+ * Masks all prohibited characters in a given string
+ * @param input string to replace all dnp prohibited chars
+ * @returns string with all reserved chars masked
+ */
+function replaceProhibitedChars(input: string): string {
+    let output: string = input;
+
+    // replace each found problematic character with its decimal representation of the us-ascii code, starting with an ,=44
     prohibitedPrintableChars.forEach((char) => {
         output = output.replaceAll(
             char,
@@ -54,6 +68,30 @@ export function encode(input: string): string {
         );
     });
 
+    return output;
+}
+
+
+/**
+ * Masks all characters forbidden by DIN 91406
+ * @param input string to encode into dnp-encoding
+ * @returns string with all reserved & prohibited printable characters masked
+ */
+export function encode(input: string): string {
+    let output = input;
+
+    // replace each found problematic character (including UTF-8) with its decimal representation of the us-ascii code, starting with an ,=44
+    output = encodeURI(replaceProhibitedChars(replaceReservedChars(output)));
+    
+    // if the url encode did encode some elements one needs to mask the %
+    if (prohibitedPrintableChars.some(elem => {
+        if (output.includes(elem)) {
+            return true;
+        } else {
+            return false;
+        }
+    })) output = replaceProhibitedChars(output);
+    
     return output;
 }
 
@@ -66,7 +104,7 @@ export function decode(input: string): string {
     // match all 2 hex digits after each comma, get it as iterator and flatten the array as we only have one element per match with our regex
     const encHexDigits = [...input.matchAll(/(?<=,)\S{2}/g)].flat();
 
-    let output = input;
+    let output = decodeURIComponent(input);
     encHexDigits.forEach((elem) => {
         // we add the comma only for matching in the string so the encHexDigits array is clean and can directly be converted to ASCII
         output = output.replaceAll(
