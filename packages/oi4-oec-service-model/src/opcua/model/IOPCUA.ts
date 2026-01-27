241601fbdc88ed5016e9725348630faae6dac7f9
thomas.weinschenk <thomas.weinschenk@endress.com>
2026-01-27 10:55:43 +0100

    Reviewed and updated ADR005

--- packages/oi4-oec-service-model/src/opcua/model/IOPCUA.ts
+++ packages/oi4-oec-service-model/src/opcua/model/IOPCUA.ts
@@ -21,19 +21,13 @@ export interface IMasterAssetModel {
 
 /**
  * OPC UA PubSub NetworkMessage structure.
- * ADR 005: CorrelationId updated for OPC UA Part 14 compliance.
  */
 export interface IOPCUANetworkMessage {
   MessageId: MessageId;
   MessageType: EOPCUAMessageType;
   PublisherId: string; // TODO: string in the format <serviceType>/<appId>, need to add validators
   DataSetClassId: GUID;
-  /**
-   * ADR 005: Optional correlation identifier for request-response matching.
-   * Used to correlate response messages to their original request.
-   * Follows OPC UA Part 14-7.2.2.1 specification.
-   */
-  CorrelationId?: MessageId;
+  ReplyTo?: MessageId;
   Messages: IOPCUADataSetMessage[]; // TODO: This should be generic (either Messages or MetaData)
 }
 
